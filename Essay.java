e366b8bd9bf600570cbe7d70aa7a435e4421d146
edinsonjea <edinsonjea@gmail.com>
2022-08-11 21:01:04 -0500

    Realice mi primer cambio en la linea 7

--- Essay.java
+++ Essay.java
@@ -2,7 +2,8 @@ public class Essay {
     public static void main(String args[])
     {
         System.out.println("Hola");
-        System.out.println("Hola mi nombre es: Juliana Parra Gonzalez y este es mi primer cambio");
+        System.out.println("Grupo");
         System.out.println("Hola mi nombre es: Luisa María Álvarez García y este es mi primer cambio");
+        System.out.println("Hi Five Group, Mi nombre es Edinson Jose Espitaleta Arrieta esta es mi primera linea de codigo en java");
     }
 }
