c71c545a8b59bf8d8d522da1d26629d24283a289
julianapg <julianapg92@gmail.com>
2022-08-11 20:43:45 -0500

    Primer cambio de Juliana

--- Essay.java
+++ Essay.java
@@ -2,6 +2,6 @@ public class Essay {
     public static void main(String args[])
     {
         System.out.println("Hola");
-        System.out.println("Grupo");
+        System.out.println("Hola mi nombre es: Juliana Parra Gonzalez y este es mi primer cambio");
     }
 }
